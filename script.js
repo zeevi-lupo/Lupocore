5f5b4046ec6924e61b2d413fffdcb0b4fb3563ae
Zeevi <zeevi.github@gmail.com>
2026-07-27 19:29:00 +0700

    Update script.js

--- script.js
+++ script.js
@@ -324,7 +324,8 @@ const linksAdblueData = {
     'reno': { 'travel town' :"https://tinyurl.com/476nasyy", 'gossip harbor': "https://tinyurl.com/muaaas7n"}, 
     'galon': {'travel town':"https://tinyurl.com/y3kb3y4s", 'gossip harbor': "https://tinyurl.com/4ue4v3cj"}, 
     'jadud': {'gossip harbor':" https://tinyurl.com/2nz3j4yj", 'travel town': "https://tinyurl.com/4mdyzz47", 'monopoly go': "https://tinyurl.com/5n8e34pa"}, 
-    'panci': {'gossip harbor':"https://tinyurl.com/yc74x2w3", 'travel town': "https://tinyurl.com/53fjx996"}
+    'panci': {'gossip harbor':"https://tinyurl.com/yc74x2w3", 'travel town': "https://tinyurl.com/53fjx996"}, 
+    'joni': {'travel town': "https://tinyurl.com/552c8n3v"}
 };
 
 function openLink(id, nama) {
