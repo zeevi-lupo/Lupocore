c0a9a639cfaa1fa49824e32dc3c26a42d02835c7
Zeevi <zeevi.github@gmail.com>
2026-08-24 19:54:17 +0700

    Update script.js

--- script.js
+++ script.js
@@ -336,7 +336,7 @@ const linksAdblueData = {
     'galon': {'travel town':"https://tinyurl.com/y3kb3y4s", 'gossip harbor': "https://tinyurl.com/4ue4v3cj",'coin master':"https://tinyurl.com/43w29j5t"}, 
     'jadud': {'gossip harbor':" https://tinyurl.com/2nz3j4yj", 'travel town': "https://tinyurl.com/4mdyzz47", 'monopoly go': "https://tinyurl.com/5n8e34pa", 'coin master':"https://tinyurl.com/4th7mab8"}, 
     'panci': {'gossip harbor':"https://tinyurl.com/yc74x2w3", 'travel town': "https://tinyurl.com/53fjx996", 'coin master':"https://tinyurl.com/3mtbk6sx"}, 
-    'joni': {'gossip harbor':"https://tinyurl.com/3ps26pjt", 'travel town': "https://tinyurl.com/552c8n3v", 'coin master:"https://tinyurl.com/3mxn6e8h"},
+    'joni': {'gossip harbor':"https://tinyurl.com/3ps26pjt", 'travel town': "https://tinyurl.com/552c8n3v", 'coin master':"https://tinyurl.com/3mxn6e8h"},
     'megalodon': {'gossip harbor': "https://tinyurl.com/yu8umu69", 'coin master':"https://tinyurl.com/3npn94jv"}
 };
 
